bf1f86c52ed53365af567bd9044d65cd4f42a1b5
RizkyFahrizal <m.rizkyfahrizal1410@gmail.com>
2021-07-18 19:49:00 +0700

    Android-PBO 5-10
    minggu #2

--- Kalkulator/app/src/main/java/com/smkrevit/kalkulator/MainActivity.java
+++ Kalkulator/app/src/main/java/com/smkrevit/kalkulator/MainActivity.java
@@ -3,12 +3,47 @@ package com.smkrevit.kalkulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText bil_1,bil_2;
+    TextView hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void hitung(View view) {
+        bil_1=findViewById(R.id.etBil_1);
+        bil_2=findViewById(R.id.etBil_2);
+        hasil=findViewById(R.id.tvHasil);
+
+        if (bil_1.getText().toString().equals("")){
+            bil_1.setError("harus di isi");
+        }
+        else if (bil_2.getText().toString().equals("")){
+                bil_2.setError("harus di isi");
+        }
+        else {
+                Double a =Double.parseDouble(bil_1.getText().toString());
+                Double b =Double.parseDouble(bil_2.getText().toString());
+                Kalkulator calc= new Kalkulator();
+
+                double c = calc.perkalian(a,b);
+
+                hasil.setText(c +"");
+            }
+
+
     }
 }
